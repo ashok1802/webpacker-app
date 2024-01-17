c57427b93f64249d1c02dde5fb534ebcfbe56c64
Ashok Kori <ashok@joshs-MacBook-Pro-2.local>
2024-01-17 13:27:01 +0530

    Added custom files

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,8 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import "src/example.js"
+import "src/custom_file_1"
+import "src/custom_file_2"
 
 Rails.start()
 Turbolinks.start()
---
+++ app/javascript/src/custom_file_2.js
@@ -0,0 +1,1 @@
+alert("Custom file 2 in Webpacker!");
